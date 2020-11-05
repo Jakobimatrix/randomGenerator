27bb85371b7761b0b9814b57d494d7fb3041da4e
Jakob Wandel <Sonnentau.koenig@gmail.com>
2020-11-05 20:33:24 +0100

    fix class, add commentary, more example

--- example.cpp
+++ example.cpp
@@ -9,19 +9,18 @@ int main() {
   tool::RandomGenerator* rg;
   rg = &rg->getInstance();
 
+  // *********** distributions ***********
+
+  std::cout << " some random numbers:\n";
   std::cout << rg->normalDistribution(0., 1.) << std::endl;
   std::cout << rg->uniformDistribution(0, 1) << std::endl;
   std::cout << rg->zeroOne() << std::endl;
   std::cout << rg->trueFalse() << std::endl;
 
-  std::array<int, 3> a;
-  std::vector<int> b;
-  b.reserve(4);
-
-
-
   constexpr int NUM_VALS = 10;
   constexpr int NUM_EXPERIMENTS = 10000;
+
+  std::cout << "\na Uniform [0,9] distribution:\n";
 
   std::array<int, NUM_VALS> num_count;
   for (std::size_t i = 0; i < NUM_VALS; i++) {
@@ -34,7 +33,26 @@ int main() {
     num_count[random_number]++;
   }
 
-  for (const int& v : num_count) {
+  for (int& v : num_count) {
+    const int num_ticks = v / static_cast<int>(0.01 * NUM_EXPERIMENTS);
+    std::cout << "|";
+    for (int j = 0; j < num_ticks; j++) {
+      std::cout << "#";
+    }
+    std::cout << v << std::endl;
+    v = 0;
+  }
+
+  std::cout << "\na N(4,1.2) distribution:\n";
+  tool::NormalDistribution nd(4., 1.2);
+  for (int i = 0; i < NUM_EXPERIMENTS; i++) {
+    int random_number = std::round(nd.get());
+    if (random_number > -1 && random_number < 10) {
+      num_count[random_number]++;
+    }
+  }
+
+  for (int& v : num_count) {
     const int num_ticks = v / static_cast<int>(0.01 * NUM_EXPERIMENTS);
     std::cout << "|";
     for (int j = 0; j < num_ticks; j++) {
@@ -42,6 +60,30 @@ int main() {
     }
     std::cout << v << std::endl;
   }
+
+
+  // *********** rand iterators ***********
+  std::array<int, 3> a = {{1, 2, 3}};
+  std::vector<int> b;
+  b.reserve(4);
+  for (int i = 0; i < 4; i++) {
+    b.emplace_back(ud.get());
+  }
+
+  std::cout << "and now a random element of an array:\n[";
+
+  for (const auto& i : a) {
+    std::cout << i << ", ";
+  }
+  std::cout << "]\nchoose [" << *(rg->get<std::array<int, 3>>(a)) << "]" << std::endl;
+
+  std::cout << "and now a random element of an vector:\n[";
+
+  for (const auto& i : b) {
+    std::cout << i << ", ";
+  }
+  std::cout << "]\nchoose [" << *(rg->get<std::vector<int>>(b)) << "]" << std::endl;
+
 
 
   return 0;
--- include/randomGenerator.hpp
+++ include/randomGenerator.hpp
@@ -17,17 +17,31 @@ class RandomGenerator {
       : std::true_type {};
 
  private:
+  /*!
+   * \brief private Constructor initializing the generator.
+   */
   RandomGenerator() { generator = std::default_random_engine(rd()); }
+
   // Stop the compiler generating methods of copy the object
   RandomGenerator(RandomGenerator const& copy);             // Not Implemented
   RandomGenerator& operator=(RandomGenerator const& copy);  // Not Implemented
 
  public:
+  /*!
+   * \brief private Get the one instance ot the class
+   * \return A reference to the one existing instance of this class.
+   */
   static RandomGenerator& getInstance() {
     static RandomGenerator instance;
     return instance;
   }
 
+  /*!
+   * \brief Get a random number between min and max following a uniform distribution.
+   * \param min The lower bound of the uniform distribution
+   * \param max The higher bound of the uniform distribution.
+   * \return A random value between min and max of type T.
+   */
   template <class T>
   T uniformDistribution(T min, T max) {
     static_assert(std::is_arithmetic<T>::value, "The type T must be numeric");
@@ -45,6 +59,12 @@ class RandomGenerator {
         false);
   }
 
+  /*!
+   * \brief Get a random number according to a normal distribution.
+   * \param mean The mean value of the normal distribution.
+   * \param variance The variance of the normal distribution (== the second central moment == standard deviation squared).
+   * \return A random value between -inf and inf of type T.
+   */
   template <class T = double>
   T normalDistribution(T mean, T variance) {
     static_assert(std::is_floating_point<T>::value,
@@ -53,6 +73,10 @@ class RandomGenerator {
     return distribution(generator);
   }
 
+  /*!
+   * \brief Returns true or false with probablity of 1/2.
+   * \return A randomly true or false with probablity of 1/2.
+   */
   bool trueFalse() {
     if (zeroOne()) {
       return true;
@@ -60,23 +84,38 @@ class RandomGenerator {
     return false;
   }
 
+  /*!
+   * \brief Returns 0 or 1 with probablity of 1/2.
+   * \return A randomly 1 or 0 with probablity of 1/2.
+   */
   int zeroOne() {
     std::uniform_int_distribution<int> distribution(0, 1);
     return distribution(generator);
   }
 
+  /*!
+   * \brief Returns a random iterator given an iterator range using uniform distribution.
+   * \param first the left (boundary) iterator
+   * \param last the right (boundary) iterator
+   * \return A random iterator between (including) first and last.
+   */
   template <typename InputIt>
-  static typename std::enable_if<is_iterator<InputIt>::value, InputIt>::type get(InputIt first,
-                                                                                 InputIt last) {
-    const auto size = std::distance(first, last);
+  typename std::enable_if<is_iterator<InputIt>::value, InputIt>::type get(InputIt first,
+                                                                          InputIt last) {
+    const int size = std::distance(first, last);
     if (0 == size)
-      return last;
+      return first;
+    const auto rand_index = uniformDistribution<int>(0, size - 1);
     using diff_t = typename std::iterator_traits<InputIt>::difference_type;
-    return std::next(first, get<diff_t>(0, size - 1));
+    return std::next(first, static_cast<diff_t>(rand_index));
   }
 
+  /*!
+   * \brief Returns a random iterator given an container which one can iterate through.
+   * \return A random iterator pointing at a element in the given container.
+   */
   template <typename Container>
-  static auto get(Container& container) ->
+  auto get(Container& container) ->
       typename std::enable_if<is_iterator<decltype(std::begin(container))>::value,
                               decltype(std::begin(container))>::type {
     return get(std::begin(container), std::end(container));
@@ -87,34 +126,69 @@ class RandomGenerator {
   std::default_random_engine generator;
 };
 
+/*!
+ * \brief Conveniendt Class to hold one specific uniform distribution.
+ */
 template <class T = double>
 class UniformDistribution {
   using T_ = T;
 
  public:
-  UniformDistribution() { rg = rg->getInstance(); }
+  /*!
+   * \brief UniformDistribution default constructor constructs a uniform distribution between 0. and 1.
+   */
+  UniformDistribution()
+      : UniformDistribution(static_cast<T>(0.), static_cast<T>(1.)) {}
+
+  /*!
+   * \brief UniformDistribution constructor constructs a uniform distribution
+   * between min and max. \param min The left bound of the uniform distribution.
+   * \param max the right bound of the uniform distribution.
+   */
   UniformDistribution(T min, T max) : min(min), max(max) {
     rg = &rg->getInstance();
   }
 
+  /*!
+   * \brief get a random value according to the defined uniform distribution
+   * \return a value between min and max.
+   */
   T get() const { return rg->uniformDistribution<T>(min, max); }
 
   RandomGenerator* rg;
-  T min = 0.;
-  T max = 1.;
+  T min;
+  T max;
 };
 
+/*!
+ * \brief Conveniendt Class to hold one specific normal distribution
+ */
 template <class T = double>
 class NormalDistribution {
  public:
-  NormalDistribution() { rg = rg->getInstance(); };
+  /*!
+   * \brief NormalDistribution default constructor constructs a N(0,1) distribution
+   */
+  NormalDistribution()
+      : NormalDistribution(static_cast<T>(0.), static_cast<T>(1.)){};
+
+  /*!
+   * \brief NormalDistribution default constructor constructs a N(mean,var) distribution
+   * \param mean The first central moment of the distribution
+   * \param var The second central moment of the distribution
+   */
   NormalDistribution(T mean, T var) : mean(mean), var(var) {
     rg = &rg->getInstance();
   };
 
+
+  /*!
+   * \brief get a random value according to the defined normal distribution
+   * \return a value between min and max.
+   */
   T get() const { return rg->normalDistribution(mean, var); }
   RandomGenerator* rg;
-  T mean = 0;
-  T var = 1;
+  T mean;
+  T var;
 };
 }  // namespace tool
